73884cb85750d64acc2deee84c91756bbf7f57ae
maherme <manuel.hernandez.mendez@gmail.com>
2021-04-29 11:15:02 +0200

    Updated file DecoratingClasses.py

--- Scopes_Closures_Decorators/DecoratingClasses.py
+++ Scopes_Closures_Decorators/DecoratingClasses.py
@@ -109,3 +109,169 @@ res = favorite.debug()
 print(res)
 
 #%%
+
+from math import sqrt
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __abs__(self):
+        return sqrt(self.x ** 2 + self.y ** 2)
+    
+    def __repr__(self):
+        return 'Point({0}, {1})'.format(self.x, self.y)
+
+p1, p2, p3 = Point(2, 3), Point(2, 3), Point(0, 0)
+res = abs(p1)
+print(res)
+res = p1 is p2
+print(res)
+res = p1 is p3
+print(res)
+res = p1 == p2
+print(res) # Notice this happens because we have not implemented the equal method.
+
+#%%
+# Adding the equal method:
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __abs__(self):
+        return sqrt(self.x ** 2 + self.y ** 2)
+    
+    def __repr__(self):
+        return 'Point({0}, {1})'.format(self.x, self.y) 
+    
+    def __eq__(self, other):
+        if isinstance(other, Point):
+            return self.x == other.x and self.y == other.y
+        else:
+            return False
+    
+p1, p2, p3 = Point(2, 3), Point(2, 3), Point(0, 0)
+res = p1 is p2
+print(res)
+res = p1 == p2
+print(res)
+
+#%%
+# We can implement more method:
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __abs__(self):
+        return sqrt(self.x ** 2 + self.y ** 2)
+    
+    def __repr__(self):
+        return 'Point({0}, {1})'.format(self.x, self.y) 
+    
+    def __eq__(self, other):
+        if isinstance(other, Point):
+            return self.x == other.x and self.y == other.y
+        else:
+            return False
+    
+    def __lt__(self, other):
+        if isinstance(other, Point):
+            return abs(self) < abs(other)
+        else:
+            return NotImplemented
+
+p1, p2, p3 = Point(2, 3), Point(2, 3), Point(0, 0)
+res = p1 == p2
+print(res)
+res = p3 < p1
+print(res)
+res = p1 > p3
+print(res) # This works because python calculates p3 < p1, which is the same
+
+#%%
+# We can use decorators and monkey patching:
+#
+# a <= b if a < b or a == b
+# a > b if not(a<b) and a != b
+# a >= b if not(a<b)
+
+def complete_ordering(cls):
+    if '__eq__' in dir(cls) and '__lt__' in dir(cls):
+        cls.__le__ = lambda self, other: self < other or self == other
+        cls.__gt__ = lambda self, other: not(self < other) and not(self == other)
+        cls.__ge__ = lambda self, other: not(self < other)
+    return cls
+
+@complete_ordering
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __abs__(self):
+        return sqrt(self.x ** 2 + self.y ** 2)
+    
+    def __repr__(self):
+        return 'Point({0}, {1})'.format(self.x, self.y) 
+    
+    def __eq__(self, other):
+        if isinstance(other, Point):
+            return self.x == other.x and self.y == other.y
+        else:
+            return False
+    
+    def __lt__(self, other):
+        if isinstance(other, Point):
+            return abs(self) < abs(other)
+        else:
+            return NotImplemented
+
+p1, p2, p3, p4 = Point(2, 3), Point(2, 3), Point(0, 0), Point(100, 200)
+res = p1 <= p4
+print(res)
+res = p4 >= p2
+print(res)
+res = p1 != p2
+print(res)
+
+#%%
+# It exists a decorator in the standard library for doing the above:
+
+from functools import total_ordering
+
+@total_ordering
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __abs__(self):
+        return sqrt(self.x ** 2 + self.y ** 2)
+    
+    def __repr__(self):
+        return 'Point({0}, {1})'.format(self.x, self.y) 
+    
+    def __eq__(self, other):
+        if isinstance(other, Point):
+            return self.x == other.x and self.y == other.y
+        else:
+            return False
+    
+    def __lt__(self, other):
+        if isinstance(other, Point):
+            return abs(self) < abs(other)
+        else:
+            return NotImplemented
+
+p1, p2, p3, p4 = Point(2, 3), Point(2, 3), Point(0, 0), Point(100, 200)
+res = p1 <= p2
+print(res)
+res = p1 >= p4
+print(res)
+res = p1 != p2
+print(res)
